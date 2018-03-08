3dfacd9ba6f7c05ae2263813336eec8ff3d25379
boyko.amarov <boyko.amarov@gmail.com>
2018-03-08 20:23:39 +0200

    Fixed wrong solution in Problem_1.R

--- Problem_1.R
+++ Problem_1.R
@@ -1,42 +1,70 @@
-##############################
-## Problem set 2, Problem 1 ##
-##############################
+# install.packages(c('dplyr', 'ggplot2'))
 
-# install.packages(c('ggplot2', 'dplyr'))
 library(ggplot2)
 library(dplyr)
 
-# a)
-# Simulate 100 draws from a normal distribution with parameters
-# mu = 10, sigma^2 = 4
-# rnorm accepts a parameter sd (standard deviation, i.e. the square root of the variance).
-# To draw from N(10, 4) we call rnorm(n, mean = 10, sd = 2) and NOT rnorm(n, mean = 10, sd = 4)
+# a) Download and read the data
+pop <- read.csv('http://econometrics2018.s3-website.eu-central-1.amazonaws.com/data/cex16pop.csv')
+
+# Check the structure of the resulting data frame
+str(pop)
+summary(pop)
 
 
-x <- rnorm(n = 20, mean = 10, sd = 2)
-ggplot(data = data.frame(x), aes(x = x)) + geom_histogram()
+# b) Summarise 'sex' using a bar chart
 
+ggplot(data = pop, aes(x = sex)) + geom_bar()
 
-# b)
+# c) Summarise 'taxes' using a histogram
 
-x <- rnorm(n = 10000, mean = 10, sd = 2)
-ggplot(data = data.frame(x), aes(x = x)) + geom_histogram()
+ggplot(data = pop, aes(x = taxes)) + geom_histogram(bins=50)
 
-# c)
+# d) ATA
 
-n <- 10000
+ATA <- mean(pop$taxes)
+
+# e) Draw a sample
+
+sampleTaxes <- sample(pop$taxes, 100, replace = TRUE)
+str(sampleTaxes)
+
+estATA1 <- mean(sampleTaxes)
+estATA1
+
+# f) Estimate ATA from the sample
 R <- 1000
+n <- 1000
 
-x <- rnorm(n * R, mean = 10, sd = 2)
+simTaxes <- sample(pop$taxes, n*R, replace = TRUE)
+samplesData <- data.frame(taxes = simTaxes, r = rep(1:R, each = n))
+groupedSamples <- group_by(samplesData, r)
+estATAbySample <- summarise(groupedSamples, estATA = mean(taxes))
 
-simData <- data.frame(x = x, r = rep(1:R, each = n))
-# simData
+# g) 
 
-groupedBySimIndex <- group_by(simData, r)
-groupedBySimIndex
-estimatedMeans <- summarise(groupedBySimIndex, estMean = mean(x))
-estimatedMeans
+ggplot(data = estATAbySample, aes(x = estATA)) + geom_histogram()
 
-summary(estimatedMeans$estMean)
 
-ggplot(data = estimatedMeans, aes(x = estMean)) + geom_histogram() + xlim(c(9, 11))
+# h) Change the value of n above and re-run the code to see the distribution change
+
+# i)
+# Estimate the standard deviation 
+
+s <- sd(sampleTaxes)
+
+# The 95\% confidence interval is given by:
+
+c(estATA1 - 1.96*s/sqrt(100), estATA1 + 1.96*s/sqrt(100))
+
+# j)
+estATAbySample <- summarise(groupedSamples, 
+                            estATA = mean(taxes), 
+                            s = sd(taxes))
+samplesData <- within(estATAbySample, {
+  confIntLower <- estATA - 1.96*s/sqrt(n)
+  confIntUpper <- estATA + 1.96*s/sqrt(n)
+  trueATA <- ATA
+  inConfidenceInterval <- trueATA >= confIntLower & trueATA <= confIntUpper
+})
+
+table(samplesData$inConfidenceInterval)
