ce66c607999556df135c2ca417046c19c57da25a
Boyko Amarov <boyko.amarov@gmail.com>
2018-04-23 10:19:02 +0300

    Update Template_CI.R

--- Template_CI.R
+++ Template_CI.R
@@ -9,32 +9,32 @@ pop <- read.csv('http://econometrics2018.s3-website.eu-central-1.amazonaws.com/d
 
 ## Draw a sample of persons' taxes with replacement (n = 50)
 
-sampleTaxes <- sample(pop$taxes, size = ???, replace = TRUE)
+sampleTaxes <- sample(pop$taxes, size = 50, replace = TRUE)
 
 ## Compute a 95\% normal CI for the population mean with the sample in sampleTaxes
 
-lowerCIlimit <- 
-upperCIlimit <- 
+lowerCIlimit <- mean(sampleTaxes) - sqrt(var(pop$taxes)) * qnorm(0.975) / sqrt(50)
+upperCIlimit <- mean(sampleTaxes) + sqrt(var(pop$taxes)) * qnorm(0.975) / sqrt(50)
 
 ## Compute a 95\% t CI for the population mean with the sample in sampleTaxes
 
-upperTCIlimit <- 
-lowerTCIlimit <- 
+lowerTCIlimit <- mean(sampleTaxes) - sqrt(var(sampleTaxes)) * qt(0.975, df = 50 - 1) / sqrt(50)
+upperTCIlimit <- mean(sampleTaxes) + sqrt(var(sampleTaxes)) * qt(0.975, df = 50 - 1) / sqrt(50)
 
 ## Select 1000 samples of size 50 with replacement from the taxes column in pop.
 
 R <- 1000
-n <- 
+n <- 50
 
-samples <- sample(???, size = ???, replace = TRUE)
+samples <- sample(pop$taxes, size = R * n, replace = TRUE)
 
 ## create a data frame with the sample values and a sample index
-samplesData <- data.frame(x = , sampleIndex = )
+samplesData <- data.frame(x = samples, sampleIndex = rep(1:R, each = n))
 
-groupedSamplesData <- group_by(samplesData, ???)
-samplesSummary <- summarise(???,
-   sampleMean = ???,
-   sampleStdDev = ???,
+groupedSamplesData <- group_by(samplesData, sampleIndex)
+samplesSummary <- summarise(groupedSamplesData,
+   sampleMean = mean(x),
+   sampleStdDev = sqrt(var(x)),
 )
 
 ## Compute 95\% t-confidence interval for each sample
@@ -44,20 +44,25 @@ samplesSummary <- summarise(???,
 
 samplesSummary <- within(samplesSummary, {
   ## Lower normal CI limit
-  
+  normalCIlower <- sampleMean - sqrt(var(pop$taxes))*qnorm(0.975) / sqrt(n)
   ## Upper normal CI limit
-  
+  normalCIupper <- sampleMean + sqrt(var(pop$taxes))*qnorm(0.975) / sqrt(n)
   ## Indicator 
   
   ## Lower t CI limit
-  
+  tCIlower <- sampleMean - sampleStdDev * qt(0.975, df = n - 1) / sqrt(n)
   ## Upper t CI limit
-  
+  tCIupper <- sampleMean + sampleStdDev * qt(0.975, df = n - 1) / sqrt(n)
+   
+  inCI <- mean(pop$taxes) < tCIupper & mean(pop$taxes) > tCIlower
 })
 
-## Compute the average length of the normal CI intervals in the simulation.
+table(samplesSummary$inCI)
 
-## Compute the average length of the t CI intervals in the simulation.
+## Examine the distributional assumption of the t confidence intervals
+## using a qq-plot
+
+ggplot(data = data.frame(y  = sampleTaxes)) + stat_qq(aes(sample = y))
 
 ## Visualise the first 20 confidence intervals
 
